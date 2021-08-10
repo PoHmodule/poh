18b64b4c98f766e6d65c27a15d986a1ff7cb77ef
PoHmodule <87804853+PoHmodule@users.noreply.github.com>
2021-08-11 00:18:31 +0300

    Create Love.py

---
+++ Love.py
@@ -0,0 +1,31 @@
+from .. import loader
+from asyncio import sleep 
+
+def register(cb):
+ cb(loveMod()) 
+ 
+class loveMod(loader.Module):
+ """Love"""
+ strings = {'name': 'kozyavkamod'}
+        
+ async def lvcmd(self, message):
+  """Используй .lv"""
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n\n\n\n\n\n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n    ♥️    ♥️     ♥️      ♥️    ♥️   ♥️\n\n\n\n\n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n    ♥️    ♥️     ♥️     ♥️     ♥️   ♥️\n♥️    ♥️    ♥️    ♥️    ♥️    ♥️       \n\n\n\n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit(' 🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n   ♥️    ♥️     ♥️     ♥️     ♥️   ♥️\n♥️    ♥️    ♥️    ♥️    ♥️    ♥️       \n    ♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n\n\n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n    ♥️    ♥️     ♥️     ♥️     ♥️   ♥️\n♥️    ♥️    ♥️    ♥️    ♥️    ♥️       \n    ♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n\n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n    ♥️    ♥️     ♥️     ♥️     ♥️   ♥️\n♥️    ♥️    ♥️    ♥️    ♥️    ♥️       \n    ♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n  ♥️      ♥️    ♥️  ♥️      ♥️  ♥️ \n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍') 
+  await sleep(1.25)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n\n♥️    ♥️    ♥️    ♥️    ♥️    ♥️       \n    ♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n  ♥️      ♥️    ♥️  ♥️      ♥️  ♥️  \n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n\n\n    ♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n  ♥️      ♥️    ♥️  ♥️      ♥️  ♥️ \n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n\n\n\n♥️    ♥️    ♥️    ♥️    ♥️    ♥️     \n  ♥️      ♥️    ♥️  ♥️      ♥️  ♥️ \n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
+  await sleep(0.75)
+  await message.edit('🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️🌧️🌩️⛈️\n\n\n\n\n  ☔☔☔☔☔☔☔☔☔☔☔ \n🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍🧍')
