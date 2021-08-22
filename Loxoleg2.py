9639b0e8e0be20d69e0f66abf53f539b2785c25f
PoHmodule <87804853+PoHmodule@users.noreply.github.com>
2021-08-22 23:47:26 +0300

    Update and rename Loxoleg.py to Loxoleg2.py

---
+++ Loxoleg2.py
@@ -0,0 +1,13 @@
+from .. import loader
+from asyncio import sleep
+@loader.tds
+class HeartsMod(loader.Module):
+ strings = {"name": "loxoleg"}
+ @loader.owner
+ async def oglohcmd(self, message):
+  for _ in range(1):
+   for heart in ['это' ,'это Олег' ,'это Олег и' 
+,'это Олег и он' ,'это Олег и он сдохнет' ,'это Олег и он сдохнет со','это Олег и он сдохнет со своими'
+,'это Олег и он сдохнет со своими коровами в одиночестве нахуй]:
+    await message.edit(heart)
+    await sleep(0.5)
